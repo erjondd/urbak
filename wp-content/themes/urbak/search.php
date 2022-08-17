84f58c99a97221d37803bcf3251d29ad1ccc67b2
hajrullah.islami@gmail.com <hajrullah.islami@gmail.com>
2022-08-17 17:10:41 +0200

    [fix] - Search page

--- wp-content/themes/urbak/search.php
+++ wp-content/themes/urbak/search.php
@@ -1,50 +1,85 @@
 <?php
-
-get_header();
-?>
-
-<?php
 global $wp_query;
-
-echo $query_string;
-$query_args = explode(" ", $query_string);
-$search_query = array();
-
-foreach ($query_args as $key => $string) {
-    $query_split = explode("=", $string);
-    $search_query[$query_split[0]] = $query_split[1];
-} // foreach
-
-$search = new WP_Query($search_query);
-
-$loop = new WP_Query($search);
-var_dump($loop);
-$posts = $search->posts;
-
-foreach ($posts as $post) {
-
-    print_r($post);
-
-    if ($post->post_type === "page") {
-?>
-        <div class="search-found">
-
-            <h3>Page found<h3>
-                    <div class="search-found-content">
-                        <a href="<?= the_permalink() ?>"><?= $post->post_title ?></a>
-                    </div>
-        </div>
-
-<?php
+$the_keys = preg_split('/\s+/', str_replace('-', ' ', get_query_var('s')), -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+$total_keys = count($the_keys);
+$the_query = new WP_Query(array('post_type' => 'nothing'));
+if ($total_keys > 1) {
+    for ($i = 0; $i <= $total_keys; $i++) {
+        $the_query_mask = new WP_Query(array('s' => $the_keys[$i]));
+        $the_query->post_count = count($the_query->posts);
+        $the_query->posts = array_merge($the_query->posts, $the_query_mask->posts);
     }
-    print_r($post);
+} else {
+    $the_query = new WP_Query(array('s' => get_query_var('s')));
 }
 
-
-
-
-wp_reset_postdata();
+get_header();
+$has_links = false;
+$has_products = false;
 ?>
+<section class="search-section">
+    <div class="container">
+        <h3>Links</h3>
+        <div class="search-links">
+            <div class="search-link">
+                <?php
+                foreach ($the_query->posts as $post) {
+                    if ($post->post_type === "page") {
+                        $has_links = true;
+                ?>
+                        <div class="search-link">
+                            <div class="search-found-content">
+                                <a href="<?= the_permalink() ?>"><?= $post->post_title ?></a>
+                            </div>
+                        </div>
+
+                <?php
+                    }
+                }
+                wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+        <?php if (!$has_links) { ?>
+            <div class="no-data">No links found</div>
+        <?php } ?>
+    </div>
+</section>
+
+<section class="search-section">
+    <div class="container">
+        <h3>Products</h3>
+        <div class="search-products">
+            <?php
+            foreach ($the_query->posts as $post) {
+                if ($post->post_type === "product") {
+                    $has_products = true;
+                    $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
+            ?>
+                    <div class="search-product">
+                        <a href="<?= the_permalink() ?>">
+                            <div class="search-product-image">
+                                <img src="<?= $image[0] ?>" />
+                            </div>
+                            <div class="search-found-content">
+                                <span><?= $post->post_title ?></span>
+                                <span><img src="<?= get_template_directory_uri() ?>/images/arrow-right.svg" /></span>
+                                 
+                            </div>
+                        </a>
+                    </div>
+
+            <?php
+                }
+            }
+            wp_reset_postdata();
+            ?>
+        </div>
+        <?php if (!$has_products) { ?>
+            <div class="no-data">No products found</div>
+        <?php } ?>
+    </div>
+</section>
 
 <?php
 
